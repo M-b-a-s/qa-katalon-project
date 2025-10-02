dd056e78dd521fb1cc4b22baadfa0e1bb66e2035
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-10-02 03:42:08 +0100

    capture object

---
+++ Test Suites/ExploreCURA.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ExploreCURA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6d713990-83e4-41ff-ac77-81b6b9f1525e</testSuiteGuid>
+   <testCaseLink>
+      <guid>e3de4f97-e10a-4516-9397-a929df9a66a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Navigation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
